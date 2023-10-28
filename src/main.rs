d76ccba987b577884f542f38688eb244c0b13c75
Dave Gill <d.gill@stem.org.uk>
2023-10-28 13:42:10 +0100

    Tidying the code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use serde::Deserialize;
 use std::env;
-use std::fs::{File, OpenOptions};
-use std::io::{BufReader, Write, BufWriter};
 use std::error::Error;
+use std::fs::{File, OpenOptions};
+use std::io::{BufReader, BufWriter, Write};
 use walkdir::WalkDir;
 
 #[derive(Deserialize, Debug)]
@@ -10,29 +10,29 @@ struct Message {
     text: String,
 }
 
-
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         eprintln!("Usage: {} <directory>", args[0]);
         return Ok(());
     }
-    
+
     let directory = &args[1];
 
     let output_file_path = format!("{}.txt", directory);
-    let output_file = OpenOptions::new().create(true).write(true).truncate(true).open(&output_file_path)?;
+    let output_file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(&output_file_path)?;
     let mut writer = BufWriter::new(output_file);
-
 
     for entry in WalkDir::new(directory) {
         let entry = entry?;
-        println!("{}", entry.path().display());
         if entry.file_type().is_file() && entry.path().extension().unwrap() == "json" {
-            println!("is json file");
             let file = File::open(entry.path())?;
             let reader = BufReader::new(file);
-            
+
             match serde_json::from_reader::<_, Vec<Message>>(reader) {
                 Ok(messages) => {
                     for message in messages {
@@ -45,8 +45,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     }
-
-
 
     Ok(())
 }
