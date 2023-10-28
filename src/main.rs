a99869a3a766339f3f505a3c333641fe6b2c0236
Dave Gill <d.gill@stem.org.uk>
2023-10-28 13:13:06 +0100

    Added directory traversal

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,46 @@
 use serde::Deserialize;
+use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::error::Error;
-
+use walkdir::WalkDir;
 
 #[derive(Deserialize, Debug)]
 struct Message {
     text: String,
 }
 
-#[derive(Deserialize, Debug)]
-struct SlackExport {
-    messages: Vec<Message>,
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file = File::open("slack_export.json")?;
-    let reader = BufReader::new(file);
-//    let slack_export: SlackExport = serde_json::from_reader(reader)?;
-    let messages: Vec<Message> = serde_json::from_reader(reader)?;
-
-    for message in messages {
-        println!("{}", message.text);
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <directory>", args[0]);
+        return Ok(());
     }
+    
+    let directory = &args[1];
+    for entry in WalkDir::new(directory) {
+        let entry = entry?;
+        println!("{}", entry.path().display());
+        if entry.file_type().is_file() && entry.path().extension().unwrap() == "json" {
+            println!("is json file");
+            let file = File::open(entry.path())?;
+            let reader = BufReader::new(file);
+            
+            match serde_json::from_reader::<_, Vec<Message>>(reader) {
+                Ok(messages) => {
+                    for message in messages {
+                        println!("{}", message.text);
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            }
+        }
+    }
+
+
 
     Ok(())
 }
