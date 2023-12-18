cc7986916390ec29a768e068429bc3f00e3dd423
Dave Gill <david.g.h.gill@gmail.com>
2023-12-18 14:20:43 +0000

    Added date extracted from message as well

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use std::error::Error;
 use std::fs::{File, OpenOptions};
 use std::io::{BufReader, BufWriter, Write};
 use walkdir::WalkDir;
+use chrono::{DateTime, TimeZone, Utc};
 
 #[derive(Deserialize, Debug)]
 struct Message {
     text: String,
+    ts: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -39,7 +41,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                     match serde_json::from_reader::<_, Vec<Message>>(reader) {
                         Ok(messages) => {
                             for message in messages {
-                                writeln!(&mut writer, "{}", message.text)?;
+                                let ts_str = message.ts;
+                                let ts = ts_str.parse::<f64>()?.round() as i64;
+                                let datetime: DateTime<Utc> = Utc.timestamp(ts, 0);
+                                let date_string = datetime.format("%Y-%m-%d %H:%M:%S").to_string();
+                                writeln!(&mut writer, "{}:{}", date_string, message.text)?;
                             }
                         }
                         Err(e) => {
